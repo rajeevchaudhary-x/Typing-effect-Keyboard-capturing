b93dbb6f01f296cce79ae6e077909739ed91c73a
Rajeev choudhary <91446278+rajeevchoudhary035@users.noreply.github.com>
2022-04-16 21:49:56 +0800

    Add files via upload

---
+++ css/game.js
@@ -0,0 +1,91 @@
+var typed = new Typed('.head', {
+    strings: ["Welcome My Child!!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true
+  });
+
+  
+window.addEventListener('keydown',check);
+function check(t){
+    if(t.keyCode=='65'){
+       document.getElementById('obj').src="images/3.png";
+    }
+    if(t.keyCode=='66'){
+       document.getElementById('obj').src="images/b.png";
+    }
+    if(t.keyCode=='67'){
+       document.getElementById('obj').src="images/c.png";
+    }
+    if(t.keyCode=='68'){
+       document.getElementById('obj').src="images/d.png";
+    }
+    if(t.keyCode=='69'){
+       document.getElementById('obj').src="images/e.png";
+    }
+    if(t.keyCode=='70'){
+       document.getElementById('obj').src="images/f.png";
+    }
+    if(t.keyCode=='71'){
+       document.getElementById('obj').src="images/g.png";
+    }
+    if(t.keyCode=='72'){
+       document.getElementById('obj').src="images/h.png";
+    }
+    if(t.keyCode=='73'){
+       document.getElementById('obj').src="images/i.png";
+    }
+    if(t.keyCode=='74'){
+       document.getElementById('obj').src="images/j.png";
+    }
+    if(t.keyCode=='75'){
+       document.getElementById('obj').src="images/k.png";
+    }
+    if(t.keyCode=='76'){
+       document.getElementById('obj').src="images/l.png";
+    }
+    if(t.keyCode=='77'){
+       document.getElementById('obj').src="images/m.png";
+    }
+    if(t.keyCode=='78'){
+       document.getElementById('obj').src="images/n.png";
+    }
+    if(t.keyCode=='79'){
+       document.getElementById('obj').src="images/o.png";
+    }
+    if(t.keyCode=='80'){
+       document.getElementById('obj').src="images/p.png";
+    }
+  
+    if(t.keyCode=='81'){
+       document.getElementById('obj').src="images/q.png";
+    }
+    if(t.keyCode=='82'){
+       document.getElementById('obj').src="images/r.png";
+    }
+    if(t.keyCode=='83'){
+       document.getElementById('obj').src="images/s.png";
+    }
+    if(t.keyCode=='84'){
+       document.getElementById('obj').src="images/t.png";
+    }
+    if(t.keyCode=='85'){
+       document.getElementById('obj').src="images/u.png";
+    }
+    if(t.keyCode=='86'){
+       document.getElementById('obj').src="images/v.png";
+    }
+    if(t.keyCode=='87'){
+       document.getElementById('obj').src="images/w.png";
+    }
+    if(t.keyCode=='88'){
+       document.getElementById('obj').src="images/x.png";
+    }
+    if(t.keyCode=='89'){
+       document.getElementById('obj').src="images/y.png";
+    }
+    if(t.keyCode=='90'){
+       document.getElementById('obj').src="images/z.png";
+    }
+};
+
